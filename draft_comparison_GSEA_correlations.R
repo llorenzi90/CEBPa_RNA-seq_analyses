8a54fdcf617f512de9746a1e7d7064f5a45c5c9f
llorenzi90 <37328156+llorenzi90@users.noreply.github.com>
2021-07-02 17:40:38 +0200

    added correlation plots

--- draft_comparison_GSEA_correlations.R
+++ draft_comparison_GSEA_correlations.R
@@ -183,10 +183,42 @@ for (geneset in c("Hallmarks","c2","c3","c7")) {
   
     
 
+sessionInfo()
   
-  
-  
-    
-
-
-
+# R version 4.0.3 (2020-10-10)
+# Platform: x86_64-apple-darwin17.0 (64-bit)
+# Running under: macOS Catalina 10.15.7
+# 
+# Matrix products: default
+# BLAS:   /System/Library/Frameworks/Accelerate.framework/Versions/A/Frameworks/vecLib.framework/Versions/A/libBLAS.dylib
+# LAPACK: /Library/Frameworks/R.framework/Versions/4.0/Resources/lib/libRlapack.dylib
+# 
+# locale:
+#   [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+# 
+# attached base packages:
+#   [1] stats     graphics  grDevices utils     datasets  methods   base     
+# 
+# other attached packages:
+#   [1] ggpubr_0.4.0      data.table_1.14.0 forcats_0.5.1     stringr_1.4.0     dplyr_1.0.6      
+# [6] purrr_0.3.4       readr_1.4.0       tidyr_1.1.3       tibble_3.1.2      ggplot2_3.3.4    
+# [11] tidyverse_1.3.1  
+# 
+# loaded via a namespace (and not attached):
+#   [1] tidyselect_1.1.1 splines_4.0.3    haven_2.4.1      lattice_0.20-44  carData_3.0-4   
+# [6] colorspace_2.0-1 vctrs_0.3.8      generics_0.1.0   mgcv_1.8-36      utf8_1.2.1      
+# [11] rlang_0.4.11     pillar_1.6.1     foreign_0.8-81   glue_1.4.2       withr_2.4.2     
+# [16] DBI_1.1.1        dbplyr_2.1.1     modelr_0.1.8     readxl_1.3.1     lifecycle_1.0.0 
+# [21] ggsignif_0.6.2   munsell_0.5.0    gtable_0.3.0     cellranger_1.1.0 zip_2.2.0       
+# [26] rvest_1.0.0      rio_0.5.26       labeling_0.4.2   curl_4.3.1       fansi_0.5.0     
+# [31] broom_0.7.7      Rcpp_1.0.6       polynom_1.4-0    scales_1.1.1     backports_1.2.1 
+# [36] jsonlite_1.7.2   abind_1.4-5      farver_2.1.0     fs_1.5.0         hms_1.1.0       
+# [41] digest_0.6.27    openxlsx_4.2.4   stringi_1.6.2    rstatix_0.7.0    grid_4.0.3      
+# [46] cli_2.5.0        tools_4.0.3      magrittr_2.0.1   car_3.0-10       crayon_1.4.1    
+# [51] pkgconfig_2.0.3  ellipsis_0.3.2   Matrix_1.3-4     xml2_1.3.2       reprex_2.0.0    
+# [56] lubridate_1.7.10 assertthat_0.2.1 httr_1.4.2       rstudioapi_0.13  R6_2.5.0        
+# [61] nlme_3.1-152     compiler_4.0.3  
+#     
+# 
+# 
+# 
